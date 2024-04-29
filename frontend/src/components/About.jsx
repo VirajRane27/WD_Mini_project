3587edf382891c058a4260dd36a520cd0207267a
VirajRane27 <132334373+VirajRane27@users.noreply.github.com>
2024-04-29 14:57:23 +0530

    Add files via upload

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { HiOutlineArrowRight } from "react-icons/hi";
+
+const About = () => {
+  return (
+    <>
+      <section className="about" id="about">
+        <div className="container">
+          <div className="banner">
+            <div className="top">
+              <h1 className="heading">ABOUT US</h1>
+              <p>The only thing we're serious about is food.</p>
+            </div>
+            <p className="mid">
+              We provide Best and Healthy Food. We take care of all necessary prevention about our Hygiene.
+              Come here and Enjoy our FOOD.
+            </p>
+            <Link to={"/"}>
+              Explore Menu{" "}
+              <span>
+                <HiOutlineArrowRight />
+              </span>
+            </Link>
+          </div>
+          <div className="banner">
+            <img src="about.png" alt="about" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">VIRAJ</div>
+          <div className="right">
+            <p>A6 Arunoday Nagar, Mulund, Mumbai</p>
+            <p>Open: 09:00 AM - 10:00 PM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By VIRAJ</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved by VIRAJ.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
